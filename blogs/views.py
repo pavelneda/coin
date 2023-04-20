03d1ca5dade0141f17f915bc0c7c19a21e84b92f
pavlonedaskivskij8@gmail.com <90754825+pavelneda@users.noreply.github.com>
2023-04-20 16:07:52 +0300

    add blogs logic

--- blogs/views.py
+++ blogs/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from blogs.models import Blog
+
 
 def blogs(request):
-    return render(request, 'blogs/index.html')
+    blogs = Blog.objects.all()
+    return render(request, 'blogs/index.html', {"blogs": blogs})
 
 
 def blog(request, blog_id):
+<<<<<<< Updated upstream
     return HttpResponse(f'{blog_id} blog page')
+=======
+    blog = Blog.objects.get(pk=blog_id)
+    return render(request, 'blogs/blog.html', {"blog": blog})
+>>>>>>> Stashed changes
 
