6f4437e8883feaa35019de753f8ab16738add286
pavlonedaskivskij8@gmail.com <90754825+pavelneda@users.noreply.github.com>
2023-04-22 23:08:13 +0300

    add login and register

--- account/urls.py
+++ account/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.account),
-    path('sign-in/', views.signIn),
-    path('sign-up/', views.signUp),
+    path('', views.account, name='account'),
+    path('sign-in/', views.LoginUser.as_view(), name='signin'),
+    path('sign-up/', views.RegisterUser.as_view(), name='signup'),
+    path('logout', views.logout_user, name='logout')
 ]
--- account/views.py
+++ account/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from account.forms import RegisterUserForm, LoginUserForm
 
 
 def account(request):
-    return HttpResponse("account page")
+    return render(request, 'account/account.html')
 
 
-def signIn(request):
-    return HttpResponse("sign In page")
+def logout_user(request):
+    logout(request)
+    return redirect('signin')
 
 
-def signUp(request):
-    return HttpResponse("sign Up page")
+class RegisterUser(CreateView):
+    form_class = RegisterUserForm
+    template_name = 'account/signup.html'
+    success_url = reverse_lazy('signin')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('account')
+
+class LoginUser(LoginView):
+    form_class = LoginUserForm
+    template_name = 'account/signin.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return dict(list(context.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('account')
